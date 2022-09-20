1479181678a67eab960ccdf30da1db768d85eb03
Justin A-M <justin@omnibook.com>
2022-09-20 11:41:48 +0200

    login systelm

--- App.js
+++ App.js
@@ -4,3 +4,8 @@ import { Router } from "./router/Router";
 export default function App() {
   return <Router></Router>;
 }
+// db.createUser({
+//   user: "myUserAdmin",
+//   pwd: "abc123",
+//   roles: [{ role: "userAdminAnyDatabase", db: "admin" }],
+// });
