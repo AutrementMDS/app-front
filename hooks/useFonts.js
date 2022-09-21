64d973b9e8b72ae128fdf45cd9bd730bfd2240eb
leaPortier74 <lea.portier@hotmail.fr>
2022-09-21 09:15:32 +0200

    add other font

--- hooks/useFonts.js
+++
@@ -1,9 +0,0 @@
-// import { useFonts } from "expo-font";
-
-// export default useFonts = async () =>
-//   await Font.loadAsync({
-//     GibsonR: require("../assets/fonts/gibson-font/Gibson-Regular.ttf"),
-//     GibsonRI: require("../assets/fonts/gibson-font/Gibson-RegularItalic.ttf"),
-//     GibsonB: require("../assets/fonts/gibson-font/gibson-bold.ttf"),
-//     GibsonBI: require("../assets/fonts/gibson-font/Gibson-BoldItalic.ttf"),
-//   });
